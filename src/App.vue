59235d8a5271dad65e94373ebd667841c1909b9f
热心市民 <2054943869@qq.com>
2025-06-06 12:56:56 +0800

    commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="app">
+      <h1>根组件</h1>
+      <ToDoList></ToDoList>
+    </div>
+  </template>
+
+
+<script>
+import ToDoList from './components/ToDoList.vue';
+export default {
+    name:'App',
+    components:{
+        ToDoList
+    },
+
+};
+
+</script>
+<style>
+
+</style>
